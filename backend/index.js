8555a27a4467c0a218ad6be480711765b9c6ec06
Arman <armandwi.pangestu7@gmail.com>
2024-03-21 09:37:48 +0700

    feat: add UserModel

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import DB from "./config/DB.js";
+// import Users from "./models/UserModel.js";
 
 const app = express();
 const PORT = 5000;
@@ -9,6 +10,8 @@ dotenv.config();
 try {
     await DB.authenticate();
     console.log("Database Connected...!");
+    // Create Table from UserModel
+    // await Users.sync();
 } catch (error) {
     console.error(error);
 }
