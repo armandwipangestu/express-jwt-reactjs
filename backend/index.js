a0074cbd9072568ec67ce6257360425656775a79
Arman <armandwi.pangestu7@gmail.com>
2024-03-23 16:49:02 +0700

    feat: add cors to the API

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import DB from "./config/DB.js";
 // import Users from "./models/UserModel.js";
 import router from "./routes/index.js";
@@ -18,6 +19,9 @@ try {
     console.error(error);
 }
 
+// credentials: true (this allow user to send credentials)
+// origin: http://localhost:3000 (this is the domain client who allowed to access this API, because we run reactjs, so it run on port 3000)
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
