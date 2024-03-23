3e4a7f4dddee35b7765252ae765260a867567570
Arman <armandwi.pangestu7@gmail.com>
2024-03-23 23:01:55 +0700

    setup: frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+function App() {
+    return (
+        <>
+            <div>Hello World!</div>
+        </>
+    );
+}
+
+export default App;
