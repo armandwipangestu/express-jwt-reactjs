60ff76fecfd76cf9275aed5568978f7008589e35
Arman <armandwi.pangestu7@gmail.com>
2024-03-24 00:22:06 +0700

    feat: add handle JWT problem when accessToken expire

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,12 +1,80 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./Navbar";
+import axios from "axios";
+import { jwtDecode } from "jwt-decode";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+    const [name, setName] = useState("");
+    const [token, setToken] = useState("");
+    const [expire, setExpire] = useState("");
+
+    const navigate = useNavigate();
+
+    const refreshToken = async () => {
+        try {
+            const response = await axios.get("http://localhost:5000/token");
+            setToken(response.data.accessToken);
+
+            const decoded = jwtDecode(response.data.accessToken);
+            setName(decoded.name);
+            setExpire(decoded.exp);
+        } catch (error) {
+            if (error.response) {
+                navigate("/");
+            }
+        }
+    };
+
+    useEffect(() => {
+        refreshToken();
+    }, []);
+
+    const axiosJWT = axios.create();
+
+    // - Problem: JWT will problem when you send request to the API if the endpoint must be have a accessToken
+    //            such as middleware checking, this can happen bcause the accessToken expire have limit 15s or 20s on the payload when the jwt token sign
+    //
+    // - Solution: So with `axios.interceptors` this code below it will be send request to the refreshToken endpoint before any axios request
+    //           to generate a new accessToken sign when the accessToken is expire
+    axiosJWT.interceptors.request.use(
+        async (config) => {
+            const currentDate = new Date();
+            // expire here on miliseconds
+            if (expire * 1000 < currentDate.getTime()) {
+                const response = await axios.get("http://localhost:5000/token");
+                config.headers.Authorization = `Bearer ${response.data.accessToken}`;
+
+                setToken(response.data.accessToken);
+                const decoded = jwtDecode(response.data.accessToken);
+                setName(decoded.name);
+                setExpire(decoded.exp);
+            }
+
+            return config;
+        },
+        (error) => {
+            return Promise.reject(error);
+        }
+    );
+
+    const getUsers = async () => {
+        const response = await axiosJWT.get("http://localhost:5000/users", {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        console.log(response.data);
+    };
+
     return (
         <div>
             <Navbar />
             <div className="container mt-5">
-                <h1 className="">Welcome Back: </h1>
+                <h1 className="">Welcome Back: {name}</h1>
+                <button onClick={getUsers} className="button is-info">
+                    Get Users
+                </button>
             </div>
         </div>
     );
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,11 +9,12 @@ const Navbar = () => {
         >
             <div className="container">
                 <div className="navbar-brand">
-                    <a className="navbar-item" href="https://bulma.io">
+                    <a className="navbar-item" href="#">
                         <img
                             src="http://jwt.io/img/logo-asset.svg"
                             width="112"
                             height="28"
+                            alt="Logo"
                         />
                     </a>
 
@@ -23,6 +24,7 @@ const Navbar = () => {
                         aria-label="menu"
                         aria-expanded="false"
                         data-target="navbarBasicExample"
+                        href="/"
                     >
                         <span aria-hidden="true"></span>
                         <span aria-hidden="true"></span>
@@ -32,13 +34,17 @@ const Navbar = () => {
 
                 <div id="navbarBasicExample" className="navbar-menu">
                     <div className="navbar-start">
-                        <a className="navbar-item">Home</a>
+                        <a className="navbar-item" href="/">
+                            Home
+                        </a>
                     </div>
 
                     <div className="navbar-end">
                         <div className="navbar-item">
                             <div className="buttons">
-                                <a className="button is-light">Log Out</a>
+                                <button className="button is-light">
+                                    Log Out
+                                </button>
                             </div>
                         </div>
                     </div>
